0c12406f5d70475e847b9cd973c04aa72056407c
adeelibr <adeelimranr@gmail.com>
2019-04-29 22:48:36 +0200

    Added documentation for buildPaths

--- buildPaths.js
+++ buildPaths.js
@@ -1,8 +1,16 @@
 const path = require('path');
 
+/**
+ * Provide absolute paths for where a generated `html` & `pdf`
+ * files will be created.
+ * 
+ * `path.resolve` takes in a relative file path & returns the 
+ * absolute path.
+ */
 const buildPaths = {
 	buildPathHtml: path.resolve('./build.html'),
 	buildPathPdf: path.resolve('./build.pdf')
 };
 
-module.exports = buildPaths;
+// module.exports = buildPaths;
+export default buildPaths;
